4e6db531890b5479cdf610474ade45ac28500548
Paul Summers <paulsumm@microsoft.com>
2016-08-04 00:10:12 -0400

    More revisions of SP.REsults.js

--- SPSearchJs/SPSearchJs/SP.Query.js
+++ SPSearchJs/SPSearchJs/SP.Query.js
@@ -27,38 +27,52 @@ var Query = {
     RequestContentType: "application/json;odata=verbose" 
 }
 
+var GQLActions = {
+    PERSONAL_FEED:"1021",            //Private
+    MODIFIED:"1003",                 //Public
+    ORG_COLLEAGUES:"1015",           //Public
+    ORG_DIRECT:"1014",               //Public
+    ORG_MANAGER:"1013",              //Public
+    ORG_SKIP_LEVEL_MANAGER:"1016",   //Public
+    WORKING_WITH:"1019",             //Private
+    TRENDING_AROUND:"1020",          //Public
+    VIEWED: "1001",                   //Private
+    WORKING_WITH_PUBLIC:"1033"      //Public
+};
+
 function BuildQuery (queryParams) {
 
-        var queryExpr = "querytext='" + queryParams.QueryText.trim() + "'";
-        queryExpr += "&selectproperties='" + queryParams.SelectProperties + "'";
-        queryExpr += "&refiners='" + queryParams.Refiners + "'";
+    var queryExpr = "querytext='" + queryParams.QueryText.trim() + "'";
+    queryExpr += "&selectproperties='" + queryParams.SelectProperties + "'";
+    queryExpr += "&refiners='" + queryParams.Refiners + "'";
 
-        if (queryParams.SourceId != "") queryExpr += "&sourceid='" + queryParams.SourceId + "'";
+    if (queryParams.SourceId != "") queryExpr += "&sourceid='" + queryParams.SourceId + "'";
 
-        if (queryParams.Properties != "") queryExpr += "&properties='" + queryParams.Properties + "'";
+    if (queryParams.Properties != "") queryExpr += "&properties='" + queryParams.Properties + "'";
 
-        if (queryParams.SortFields != null && queryParams.SortFields != "") queryExpr += "&sortlist='" + queryParams.SortFields + "'";
+    if (queryParams.SortFields != null && queryParams.SortFields != "") queryExpr += "&sortlist='" + queryParams.SortFields + "'";
 
-        if (queryParams.RefinementFilters.length > 0) {
-            var refinementStr = "&refinementFilters='";
-            for (var i = 0; i < queryParams.RefinementFilters.length; i++) {
-                if (queryParams.RefinementFilters.length > 1 && i == 0) refinementStr += "and(";
-                if (i > 0) refinementStr += ",";
-                refinementStr += queryParams.RefinementFilters[i].PropertyName + ":ends-with(\"" + queryParams.RefinementFilters[i].GetDisplayString(RefinementFilters[i].FilterValue) + "\")";
-            }
-            if (queryParams.RefinementFilters.length > 1) refinementStr += ")";
-            queryExpr += refinementStr + "'";
+    if (queryParams.RefinementFilters.length > 0) {
+        var refinementStr = "&refinementFilters='";
+        for (var i = 0; i < queryParams.RefinementFilters.length; i++) {
+            if (queryParams.RefinementFilters.length > 1 && i == 0) refinementStr += "and(";
+            if (i > 0) refinementStr += ",";
+            refinementStr += queryParams.RefinementFilters[i].PropertyName + ":ends-with(\"" + queryParams.RefinementFilters[i].GetDisplayString(RefinementFilters[i].FilterValue) + "\")";
         }
+        if (queryParams.RefinementFilters.length > 1) refinementStr += ")";
+        queryExpr += refinementStr + "'";
+    }
 
-        queryExpr += "&startrow=" + queryParams.StartRow;
-        queryExpr += "&rowlimit=" + queryParams.RowLimit;
-        queryExpr += "&enablefql=" + queryParams.EnableFQL;
-        queryExpr += "&enablequeryrules=" + queryParams.EnableQueryRules;
-        queryExpr += "&enablephonetic=" + queryParams.EnablePhonetic;
-        queryExpr += "&enablestemming=" + queryParams.EnableStemming;
+    queryExpr += "&startrow=" + queryParams.StartRow;
+    queryExpr += "&rowlimit=" + queryParams.RowLimit;
+    queryExpr += "&enablefql=" + queryParams.EnableFQL;
+    queryExpr += "&enablequeryrules=" + queryParams.EnableQueryRules;
+    queryExpr += "&enablephonetic=" + queryParams.EnablePhonetic;
+    queryExpr += "&enablestemming=" + queryParams.EnableStemming;
 
-        return queryExpr;
-    }  
+    return queryExpr;
+}  
 
 exports.SPQuery = Query;
 exports.BuildQuery = BuildQuery;
+exports.GQLActions = GQLActions;
--- SPSearchJs/SPSearchJs/SP.Results.js
+++ SPSearchJs/SPSearchJs/SP.Results.js
@@ -23,7 +23,7 @@ function ParseResults(rawJson){
     if(jsonObj.PrimaryResults){
         SPResults.PrimaryResults = GetResultItems(jsonObj.PrimaryResults);
     }
-    for(var i = 0; i <= jsonObj.SecondaryResults.length; i++){
+    for(var i = 0; i < jsonObj.SecondaryResults.length; i++){
         SPResults.SecondaryResults.push(GetResultItems(jsonObj.SecondaryResults[i]));
     }
 }
@@ -35,5 +35,33 @@ function GetResultItems(jsonObj){
 
     var results = jsonObj.Table.Rows.results;
 
+    for(var i = 0; i < results.length; i++){
+        var result = results[i];
+        var fields = result.Cells.results;
 
+        var newResult = null;
+
+        var fieldJsonStr = '{';
+        for (var j = 0; j < fields.length; j++) {
+            if (fieldJsonStr.length > 1) fieldJsonStr += ",";
+
+            var theValue = fields[j].Value;
+            if (theValue != null) {
+                //Replaces line feed and carriage return characters that break the JSON parsing in IE
+                theValue = theValue.replace(/[\n\r]/g, '');
+            }
+            if (theValue != null && theValue.indexOf("[") == 0) {
+                fieldJsonStr += "\"" + fields[j].Key + "\":" + theValue;
+            }
+            else {
+                fieldJsonStr += "\"" + fields[j].Key + "\":\"" + theValue + "\"";
+            }
+            
+        }
+        fieldJsonStr += '}';
+        newResult = JSON.parse(fieldJsonStr);
+        theResults.push(newResult);
+    }
+
+    return theResults;
 }
