3b16b5f54641ba1390b61a41ed3947764cd32f4c
Paul <paul.summers@microsoft.com>
2017-06-06 21:06:23 -0400

    Basic build works - still excludes SPFXSearchClient

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                exclude: /node_modules/,
+                exclude: ['/node_modules/', '/src/ts/spfx/'],
                 use: [
                     {
                         loader: 'babel-loader',
